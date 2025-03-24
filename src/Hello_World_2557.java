4bdbe080b0220d7a2548116d808dda847368d004
김하빈 <habin81111@naver.com>
2025-03-24 15:50:06 +0900

    update: 2557

---
+++ src/Hello_World_2557.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Hello_World_2557 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World!");
+    }
+}
