14f316f31e0711876b674050515eff184a10cdcd
김하빈 <87303406+KIM-GOING@users.noreply.github.com>
2025-07-12 22:15:48 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int N = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        if (A == B) System.out.print("Anything");
+        else if (A < B) System.out.print("Bus");
+        else System.out.print("Subway");
+
+    }
+	
+}
